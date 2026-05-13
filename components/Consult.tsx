d0efd9bd707818c6723c9bab8790925a351bf145
Dawid Jura <dawidjura12@gmail.com>
2026-05-13 09:45:33 +0200

    handle rwd

--- components/Consult.tsx
+++ components/Consult.tsx
@@ -5,16 +5,26 @@ export default function Consult() {
       <div className="consult-vector" aria-hidden="true"><img src="/assets/consult-vector.svg" alt="" /></div>
       <div className="consult-photo"><img src="/assets/Darek Mikołajczak.png" alt="Dariusz Mikołajczak" /></div>
       <div className="consult-content">
-        <h2>Bezpłatna konsultacja procesu dostaw</h2>
-        <p className="lead">Możesz zapisać się na krótką konsultację z ekspertem po webinarze, aby omówić swój proces logistyczny.</p>
-        <ul>
-          <li>przeanalizujemy proces dostaw</li>
-          <li>sprawdzimy sposób dokumentowania zdarzeń</li>
-          <li>wskażemy możliwe usprawnienia</li>
-        </ul>
-        <div className="consult-person">
-          <p className="name">Dariusz Mikołajczak</p>
-          <p className="title">Business Development Manager &nbsp;- Supply Chain<br />Wieloletnie doświadczenie w branży logistycznej</p>
+        <div className="consult-content-inner">
+          <h2>Bezpłatna konsultacja procesu dostaw</h2>
+          <p className="lead">
+            Możesz zapisać się na krótką konsultację z ekspertem po webinarze, aby omówić swój proces{" "}
+            <wbr />
+            logistyczny.
+          </p>
+          <ul>
+            <li>przeanalizujemy proces dostaw</li>
+            <li>sprawdzimy sposób dokumentowania zdarzeń</li>
+            <li>wskażemy możliwe usprawnienia</li>
+          </ul>
+          <div className="consult-person">
+            <p className="name">Dariusz Mikołajczak</p>
+            <p className="title">
+              Business Development Manager &nbsp;- Supply Chain
+              <br />
+              Wieloletnie doświadczenie w branży logistycznej
+            </p>
+          </div>
         </div>
       </div>
     </section>
