53de6c3229d5e3735b7e4f9e712b18b32c6c04c2
Dawid Jura <dawidjura12@gmail.com>
2026-05-27 07:46:28 +0200

    handle rwd

--- components/Consult.tsx
+++ components/Consult.tsx
@@ -15,14 +15,19 @@ export default function Consult() {
             <li>poznasz możliwe sposoby ograniczania problemów i niejasności w łańcuchu dostaw,</li>
             <li>dowiesz się więcej o rozwiązaniach wspierających dokumentowanie i monitorowanie procesów logistycznych.</li>
           </ul>
+          <p className="consult-host-kicker">Spotkanie poprowadzi</p>
           <div className="consult-person">
             <p className="name">Dariusz Mikołajczak</p>
-            <p className="title">
-              Business Development Manager &nbsp;- Supply Chain
-              <br />
-              Wieloletnie doświadczenie w branży logistycznej
-            </p>
+            <p className="title">Business Development Manager&nbsp;- Supply Chain</p>
           </div>
+          <p className="consult-bio">
+            Posiada wieloletnie doświadczenie w zarządzaniu transformacją w obszarze Supply Chain i
+            Procurement, obejmujące zarówno środowiska korporacyjne o zasięgu globalnym, jak i
+            organizacje rozwijające się oraz projekty konsultingowe. Łączy perspektywę biznesową i
+            technologiczną koncentrując się na wspieraniu organizacji w transformacji i digitalizacji
+            łańcucha dostaw, ze szczególnym uwzględnieniem poprawy efektywności operacyjnej, redukcji
+            kosztów oraz budowy skalowalnych i odpornych na zakłócenia struktur operacyjnych.
+          </p>
         </div>
       </div>
     </section>
